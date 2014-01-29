4860c270523ef4794e67134c4dbc815f7ffa53d0
Uri Klar <uriklar@gmail.com>
2014-01-29 23:59:59 +0200

    assets precompile fix

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,22 @@ module ZikaronBasalon
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
+
+    config.assets.precompile << Proc.new { |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+
+        if ((full_path.starts_with? app_assets_path) || (full_path.starts_with? vendor_assets_path)) && (!path.starts_with? '_')
+          puts "\t" + full_path.slice(Rails.root.to_path.size..-1)
+          true
+        else
+          false
+        end
+      else
+        false
+      end
+    }
   end
 end
