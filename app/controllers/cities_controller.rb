b03d0840745243d26ff3686288afb0f2f7d305ee
uriklar <uriklar@gmail.com>
2014-04-05 17:46:45 +0300

    city delete working

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,4 +7,10 @@ class CitiesController < ApplicationController
     @city = City.find(params[:id])
     @guest = session[:guest_id] ? Guest.find(session[:guest_id]) : Guest.new
   end
+
+  def destroy
+  	@city = City.find(params[:id])
+    @city.destroy
+    redirect_to cities_path
+  end
 end
