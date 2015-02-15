8ece33016b866fdf2c54d77654a1e82784fcca01
Rubysoft <uriklar@gmail.com>
2015-02-15 20:22:23 +0200

    added guests index

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,5 +1,19 @@
 class GuestsController < ApplicationController
+	before_filter :basic_auth, only: [:index]
   def index
     @guests = Guest.all
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username,password|
+      if username == "zikaron" && password == "1234.com"
+        session[:auth] = "basic"
+        return true
+      elsif username == "zbadmin" && password == "bbznot"
+        session[:auth] = "extended"
+        return true
+      end
+      return false
+    end
   end
 end
