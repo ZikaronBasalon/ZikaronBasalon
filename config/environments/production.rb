7350834a8a1b232320098b0cfec3b2fdaec24097
Rubysoft <uriklar@gmail.com>
2015-02-27 18:36:55 +0200

    added sendgrid domain as parameter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ ZikaronBasalon::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'zikaronbasalon.herokuapp.com',
+    :domain => ENV['SENDGRID_DOMAIN'],
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
