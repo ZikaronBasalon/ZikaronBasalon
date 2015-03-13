d10a50379e5b7d642b79dea5e6c62e00cc50ccb6
Rubysoft <uriklar@gmail.com>
2015-03-13 14:36:36 +0200

    Added english version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+	private
+
+	def set_locale
+	  I18n.locale = params[:locale] if params[:locale].present?
+	  I18n.locale ||= :he
+	  # current_user.locale
+	  # request.subdomain
+	  # request.env["HTTP_ACCEPT_LANGUAGE"]
+	  # request.remote_ip
+	end
+
+	def default_url_options(options = {})
+	  {locale: I18n.locale}
+	end
 end
