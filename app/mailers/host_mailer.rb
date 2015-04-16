42d52edde9a2fd4b901839c1223dea13085c4bd8
Rubysoft <uriklar@gmail.com>
2015-04-16 19:37:40 +0300

    day after mail

--- app/mailers/host_mailer.rb
+++ app/mailers/host_mailer.rb
@@ -27,13 +27,13 @@ class HostMailer < ActionMailer::Base
   def day_before(host_id)
     @host = Host.find(host_id)
     # attachments['חומרים_מעוררי_מחשבה.pdf'] = File.read("#{Rails.root}/public/kit/thought_provoking_material.pdf")
-    attachments['ערכה_למארח.pdf'] = File.read("#{Rails.root}/public/kit/host_kit.pdf")
-    attachments['שלט_לאירוע.pdf'] = File.read("#{Rails.root}/public/kit/event_sign.pdf")
+    #attachments['ערכה_למארח.pdf'] = File.read("#{Rails.root}/public/kit/host_kit.pdf")
+    #attachments['שלט_לאירוע.pdf'] = File.read("#{Rails.root}/public/kit/event_sign.pdf")
     # attachments['תעודת_הוקרה_לאיש_עדות.pdf'] = File.read("#{Rails.root}/public/kit/certificate.pdf")
-    attachments['תעודת_הוקרה_לאיש_עדות.docx'] = File.read("#{Rails.root}/public/kit/hebrew_certificate.docx")
+    #attachments['תעודת_הוקרה_לאיש_עדות.docx'] = File.read("#{Rails.root}/public/kit/hebrew_certificate.docx")
     # attachments['שאלות_מנחות_לעדות.pdf'] = File.read("#{Rails.root}/public/kit/questions_for_witness.pdf")
     #attachments['עדויות_וסרטי_עדות.pdf'] = File.read("#{Rails.root}/public/kit/testimonials.pdf") 
     #attachments['מצגת_מפגש_הכנה_מארחים.pdf'] = File.read("#{Rails.root}/public/kit/preperation.pdf")   
-    mail :to => @host.email, :subject => "זיכרון בסלון / אנשי עדות"
+    mail :to => @host.email, :subject => "זיכרון בסלון- משוב"
   end
 end
