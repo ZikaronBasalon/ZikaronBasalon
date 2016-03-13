d441028d840a5b761e6b88223b4581ada44302ea
Uri Klar <uri.k@phytech.com>
2016-03-13 17:36:25 +0200

    removed old code

--- app/assets/javascripts/managers.show.js
+++ app/assets/javascripts/managers.show.js
@@ -78,80 +78,8 @@ app.controller('ManagerShowController', ['$scope','$uibModal', '$http', '$locati
     return filtered;
   }
     
-
-    // return _.filter(hosts, function(host) {
-
-    //   if(activeFilter($scope.search.host.survivor_needed) &&
-    //       $scope.search.host.survivor_needed !== host.survivor_needed) {
-    //     return false;
-    //   }
-
-    //   if(activeFilter($scope.search.host.has_survivor) &&
-    //       $scope.search.host.has_survivor !== host.has_survivor) {
-    //     return false;
-    //   }
-
-    //   if(activeFilter($scope.search.host.contacted) &&
-    //       $scope.search.host.contacted !== host.contacted) {
-    //     return false;
-    //   }
-
-    //   if(activeFilter($scope.search.host.city_id) && 
-    //     (!host.city || $scope.search.host.city_id !== host.city.id)) {
-    //     return false;
-    //   }
-
-    //   if ($scope.search.host.query) { 
-    //     if (!_.includes(host.user.email, $scope.search.host.query) &&
-    //         !_.includes(host.user.full_name, $scope.search.host.query) &&
-    //         !_.includes(host.address, $scope.search.host.query) &&
-    //         !_.includes(host.phone, $scope.search.host.query)
-    //     ) {
-    //       return false;
-    //     }
-    //   }
-
-    //   return true;
-    // });
-
   $scope.sort = function(arr) {
     return _.sortBy(arr, $scope.sortProp);
-  }
-
-  $scope.filterWitnesses = function(witnesses) {
-    return _.filter(witnesses, function(witness) {
-
-      if(activeFilter($scope.search.witness.witness_type) &&
-          $scope.search.witness.witness_type !== witness.witness_type) {
-        return false;
-      }
-
-      if(activeFilter($scope.search.witness.stairs) &&
-          $scope.search.witness.stairs !== witness.stairs) {
-        return false;
-      }
-
-      if(activeFilter($scope.search.witness.seminar_required) &&
-          $scope.search.witness.seminar_required !== witness.seminar_required) {
-        return false;
-      }
-
-      if(activeFilter($scope.search.witness.concept) &&
-         $scope.search.witness.concept !== witness.concept) {
-        return false;
-      }
-
-      if ($scope.search.witness.query) { 
-        if (!_.includes(witness.full_name, $scope.search.witness.query) &&
-            !_.includes(witness.witness_type, $scope.search.witness.query) &&
-            !_.includes(witness.concept, $scope.search.witness.query)
-        ) {
-          return false;
-        }
-      }
-
-      return true;
-    });
   }
 
   $scope.isAccesible = function(host) {
