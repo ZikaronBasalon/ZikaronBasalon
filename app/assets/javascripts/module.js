cdf04a19c2e8324a85d42434ae71575e44a9bf4c
Uri Klar <uri.k@phytech.com>
2016-03-24 21:58:03 +0200

    Added locale parameter to every request

--- app/assets/javascripts/module.js
+++ app/assets/javascripts/module.js
@@ -1,5 +1,17 @@
 var app = angular.module('zikaronbasalon',[
 	'ui.bootstrap'
-]).config(['$httpProvider', function ($httpProvider) {
-	$httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
+])
+.factory('httpRequestInterceptor', function () {
+  return {
+    request: function (config) {
+      config.params = config.params || {};
+      config.params.locale = document.getElementsByTagName('body')[0].className;
+      
+      return config;
+    }
+  };
+})
+.config(['$httpProvider', function ($httpProvider) {
+	$httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  $httpProvider.interceptors.push('httpRequestInterceptor');
 }]);
