212ac0d52e45434d353e14c8d369de944a128832
Uri Klar <uri.k@phytech.com>
2016-04-04 09:05:50 +0300

    Add my-profile endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
 	def new
 		@type = params[:type]
 	end
+
+	def profile
+		if current_user.nil? || current_user.meta.nil?
+			redirect_to user_session_path
+		else
+			redirect_to polymorphic_path(current_user.meta)
+		end
+	end
 end
