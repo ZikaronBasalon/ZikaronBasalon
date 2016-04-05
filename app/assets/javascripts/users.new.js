3f61e89731340d6a5c00b8543bd2aa61626aace9
Uri Klar <uri.k@phytech.com>
2016-04-05 14:44:03 +0300

    Fixed issue with redirect to he after signup

--- app/assets/javascripts/users.new.js
+++ app/assets/javascripts/users.new.js
@@ -22,7 +22,7 @@ app.controller('UserSignupController', ['$scope', '$http', function($scope, $htt
 				}
 			}).then(function(response) {
 				if(response.data && response.data.success) {
-					window.location = response.data.redirect_url
+					window.location = '/' + document.getElementById('locale').className + response.data.redirect_url
 				} else if(response.data.errors) {
 					$scope.errors = _.map(response.data.errors, function(error) {
 						return error[0];
