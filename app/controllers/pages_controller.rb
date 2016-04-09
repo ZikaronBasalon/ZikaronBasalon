82635e9022897dbbfb18d9e4ad7b93897e31056b
Uri Klar <uri.k@phytech.com>
2016-04-09 12:05:34 +0300

    fixed language filter + refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,11 @@ class PagesController < ApplicationController
   include PagesHelper
   def home
   	@hosts = Host.includes(:city, :user).where(host_conditions_hash)
-    @hosts = @hosts.select{ |h| h.available_places > 0 }
   	@hosts = @hosts.select { |h| 
-  		h.user.full_name.include?(query) ||
-  		(h.city && h.city.name.include?(query)) ||
-      (h.country && h.country.printable_name.include?(query)) ||
-  		h.address.include?(query)
-  	} if query.present?
+      h.available_places > 0 &&
+      host_in_query(h, query) &&
+      host_in_language_filter(h, params[:event_language])
+  	} 
 
     @cities = @hosts.map{ |h| h.city }.compact.uniq.sort_alphabetical_by{ |c| c[:name] }
     @countries = Country.all
@@ -36,7 +34,6 @@ private
 		h = { }
 		h[:strangers] = true
 		h[:city_id] = params[:city_id] if !params[:city_id].blank?
-		h[:event_language] = params[:event_language] if !params[:event_language].blank?
     h[:max_guests] = 1..9999
     h[:received_registration_mail] = true
     h[:country_id] = params[:country_id] if !params[:country_id].blank?
@@ -54,5 +51,24 @@ private
       arr_name = Kaminari.paginate_array(arr_name).page(page).per(10)
     end
     arr_name
+  end
+
+  def host_in_query(h, query)
+    return true if !query.present?
+    
+    h.user.full_name.include?(query) ||
+    (h.city && h.city.name.include?(query)) ||
+    (h.country && h.country.printable_name.include?(query)) ||
+    h.address.include?(query)
+  end
+
+  def host_in_language_filter(h, language)
+    return true if language.blank?
+
+    if language != 'other'
+      return h.event_language == language
+    else
+      return !['english', 'hebrew', 'arabic', 'frech', 'russian'].include?(h.event_language)
+    end
   end
 end
