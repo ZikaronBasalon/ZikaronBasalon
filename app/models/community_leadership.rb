c59cc43eb5d7846962c0c06f7918abac4ee46e7b
Uri Klar <uri.k@phytech.com>
2016-04-09 23:55:59 +0300

    hot fix

--- app/models/community_leadership.rb
+++ app/models/community_leadership.rb
@@ -6,7 +6,7 @@ class CommunityLeadership < ActiveRecord::Base
   validates_uniqueness_of :manager_id, :scope => :city_id
 
   def self.assign_manager(city, country_id)
-  	country = Country.find(country_id)
+  	country = Country.find(country_id) if country_id
   	# If country has a manager assigned and city doesnt have manager
   	if city && city.managers.count === 0 && country && country.manager
   		CommunityLeadership.create(city_id: city.id, manager_id: country.manager.id)
