eee4c69e473428696d902bf1dcfe35c6602e78ab
Uri Klar <uri.k@phytech.com>
2016-04-22 13:31:12 +0300

    send email on request cancel

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -10,7 +10,10 @@ class Invite < ActiveRecord::Base
 
   def self.cancel_overdue
     Invite.pending.each do |i|
-      i.destroy if ((Time.now.utc - i.created_at.utc) / 1.day).to_i >= 4
+      if ((Time.now.utc - i.created_at.utc) / 1.day).to_i >= 4
+        RequestMailer.request_rejected(i.id, I18n.locale).deliver 
+        i.destroy
+      end
     end
   end
 
