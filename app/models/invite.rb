49a9c789e38991fe8a1b26f0155e676c896e0ce2
Uri Klar <uri.k@phytech.com>
2016-05-04 08:37:37 +0300

    Fixes

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -11,7 +11,7 @@ class Invite < ActiveRecord::Base
 
   def self.cancel_overdue
     Invite.pending.each do |i|
-      if ((Time.now.utc - i.created_at.utc) / 1.day).to_i >= 2
+      if ((Time.now.utc - i.created_at.utc) / 1.day).to_i >= 1
         i.destroy
       end
     end
