3a7fcbe9b6b60e99b437f62bec8827ebcd73d992
Uri Klar <uri.k@phytech.com>
2016-09-22 16:22:40 +0300

    return Facebook challenge

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,8 @@
 class WebhooksController < ApplicationController
   def webhook
   	Rails.logger.info params
+  	if (params['hub.verify_token'] === 'zikaron-basalon-verify-token')
+  		render text: params['hub.challenge'], status: 200
+  	end
   end
 end
