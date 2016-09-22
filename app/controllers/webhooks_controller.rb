d76f015fff4824be6bdbe5dbe51a731f6dfb43bd
Uri Klar <uri.k@phytech.com>
2016-09-22 16:51:33 +0300

    remove verify token

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,8 +1,5 @@
 class WebhooksController < ApplicationController
   def webhook
-  	Rails.logger.info params
-  	if (params['hub.verify_token'] === 'zikaron-basalon-verify-token')
-  		render nothing: true, status: 200
-  	end
+		render nothing: true, status: 200
   end
 end
