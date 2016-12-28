a9fde394d5aecf05f85a542be05ea15e64607711
Aryeh Beitz <aryehbeitz@gmail.com>
2016-12-28 13:31:42 +0200

    fix toggle direction

--- app/assets/javascripts/managers.show.js
+++ app/assets/javascripts/managers.show.js
@@ -168,6 +168,7 @@ app.controller('ManagerShowController', ['$scope','$uibModal', '$http', '$locati
   }
 
   $scope.setSortProp = function(prop) {
+    $scope.search.reverseOrdering = !$scope.search.reverseOrdering;
     $scope.sortProp = prop;
     filter(1)
   }
