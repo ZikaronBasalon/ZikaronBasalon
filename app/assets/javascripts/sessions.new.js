3f8f6ade100c5d991f8b8cca760528604f8303fe
Aryeh Beitz <aryehbeitz@gmail.com>
2017-01-23 18:07:50 +0200

    2017 year migration without modals (also removed certain security and removed from show)

--- app/assets/javascripts/sessions.new.js
+++ app/assets/javascripts/sessions.new.js
@@ -12,9 +12,43 @@ app.controller('UserSigninController', ['$scope', '$http', function($scope, $htt
 					password: $scope.form.password,
 				}
 			}).then(function(response) {
-				if(response.status === 201) {
+				if(response.status === 201 || response.status === 200) {
 					var data = response.data;
-					window.location = '/' + document.getElementById('locale').className + '/' + data.meta_type.toLowerCase() + 's/' + data.meta_id;
+					var newrole = data.previous_meta_type;
+					var oldrole = data.meta_type;
+					var changerole = false;
+					if (newrole == null && data.meta_type == "Guest") {
+						if (confirm("Last year you were a Guest.\nPress Yes to continue being a guest or No to become a host")) {
+							newrole = oldrole;
+						} else {
+							newrole = "Host";
+							changerole = true;
+						}
+					}
+					else if (newrole == null && data.meta_type == "Host") {
+						if (confirm("Last year you were a Host.\nPress Yes to continue being a host or No to become a guest")) {
+							newrole = oldrole;
+						} else {
+							newrole = "Guest";
+							changerole = true;
+						}
+					}
+					if (changerole) {
+							$http.put('/users/' + data.id + '/assignrole.json', {
+							newrole: newrole,
+							oldrole: oldrole
+						})
+						.then(function(role_response) {
+							data = role_response.data
+							window.location = '/' + document.getElementById('locale').className + '/' + data.meta_type.toLowerCase() + 's/' + data.meta_id;
+						})
+						.catch(function(role_response) {
+							$scope.error = true;
+						});
+					}
+					if (!changerole) {
+						window.location = '/' + document.getElementById('locale').className + '/' + data.meta_type.toLowerCase() + 's/' + data.meta_id;
+					}
 				}
 			}).catch(function(response) {
 				if(response.status > 400) {
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,58 @@ class UsersController < ApplicationController
 		@type = params[:type]
 	end
 
+	def assignrole
+		user = User.find(params[:id])
+
+		# byebug
+		if params[:newrole] == "Guest" && user.host?
+			if !user.previous_meta_id.nil? && !user.previous_meta_type.nil?
+				current_meta_id = user.meta_id
+				current_meta_type = user.meta_type
+				guest = Guest.find(user.previous_meta_id)
+				user.meta_id = user.previous_meta_id
+				user.meta_type = user.previous_meta_type
+				user.meta = guest
+				user.previous_meta_id = current_meta_id
+				user.previous_meta_type = current_meta_type
+				user.save!
+			else
+				guest = Guest.create
+				guest.name = user.full_name
+				guest.email = user.email
+				guest.save!
+				user.previous_meta_id = user.meta_id
+				user.previous_meta_type = user.meta_type
+				user.meta_type = "Guest"
+				user.meta_id = guest.id
+				user.meta = guest
+				user.save!
+			end
+		elsif params[:newrole] == "Host" && user.guest?
+			if !user.previous_meta_id.nil? && !user.previous_meta_type.nil?
+				current_meta_id = user.meta_id
+				current_meta_type = user.meta_type
+				host = Host.find(user.previous_meta_id)
+				user.meta_id = user.previous_meta_id
+				user.meta_type = user.previous_meta_type
+				user.meta = host
+				user.previous_meta_id = current_meta_id
+				user.previous_meta_type = current_meta_type
+				user.save!
+			else
+				host = Host.create
+				host.save!
+				user.previous_meta_id = user.meta_id
+				user.previous_meta_type = user.meta_type
+				user.meta_id = host.id
+				user.meta_type = "Host"
+				user.meta = host
+				user.save!
+			end
+		end
+			render json: user, status: 201
+	end
+
 	def profile
 		if current_user.nil? || current_user.meta.nil?
 			redirect_to user_session_path
