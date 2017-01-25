673c3c2d4a8be673d4fbedd01cbbb88539cab3d4
Aryeh Beitz <aryehbeitz@gmail.com>
2017-01-25 13:31:33 +0200

    heroku migration error

--- db/migrate/20170124131439_add_active_this_year_to_user.rb
+++ db/migrate/20170124131439_add_active_this_year_to_user.rb
@@ -1,5 +1,5 @@
 class AddActiveThisYearToUser < ActiveRecord::Migration
   def change
-    add_column :users, :active_this_year, :boolean, :default => true
+    add_column :users, :active_this_year, :boolean, :default => false
   end
 end
