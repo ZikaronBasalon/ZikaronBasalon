21d4ae03e2234169677fae9eaefffb355209423b
Aryeh Beitz <aryehbeitz@gmail.com>
2017-04-27 19:24:47 +0300

    log everybody out

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ZikaronBasalon::Application.config.session_store :cookie_store, key: '_ZikaronBasalon_session'
+ZikaronBasalon::Application.config.session_store :cookie_store, key: '_Zikaron_Basalon_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
