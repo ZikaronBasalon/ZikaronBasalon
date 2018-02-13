1bd750a3e08f481059b6fb7b6f288a4ae5556040
avrielp <avriel.moscovitz@mail.huji.ac.il>
2018-02-13 18:20:23 +0200

    Changes so far to areas

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,5 @@
 class Region < ActiveRecord::Base
   attr_accessible :name
   belongs_to :country
-  has_many :cities, :through => :region_cities
+  # has_many :cities, :through => :region_cities
 end
