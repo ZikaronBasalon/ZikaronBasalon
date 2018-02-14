4ad00b974a984419353a994b860c4dee381ee110
avrielp <avriel.moscovitz@mail.huji.ac.il>
2018-02-14 18:33:20 +0200

    Removed irrelevant local migrations

--- db/avriel_migrate_archive/20180214092704_rename_areas_table_to_regions_table.rb
+++
@@ -1,5 +0,0 @@
-class RenameAreasTableToRegionsTable < ActiveRecord::Migration
-  def change
-    rename_table :country_areas, :country_regions
-  end
-end
--- db/avriel_migrate_archive/20180214093149_change_column_area_id_to_region_id.rb
+++
@@ -1,3 +0,0 @@
-class ChangeColumnAreaIdToRegionId < ActiveRecord::Migration
-  rename_column :country_regions, :area_id, :region_id
-end
