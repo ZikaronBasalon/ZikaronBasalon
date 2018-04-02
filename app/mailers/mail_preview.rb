696376964bf716e5d2a55c2aafd66b008113d3b3
avrielop <36268929+avrielop@users.noreply.github.com>
2018-04-02 19:42:43 +0300

    Feature/update mailers with 2018 content (#274)
    * Added an engine for previewing emails to make this task easier
    
    * Finsihed host registeration email
    
    * Finishd all host/guest emails
    
    * Updated the witness emails
    
    * Added locale to user table in DB + made it change when user changes language
    
    * Made mail preview only work under env of development
    
    * Updated last email\mailer 'event_reminder'
    
    * Changed strings with symbols to prevent issue with email content

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -2,52 +2,52 @@
 class MailPreview < MailView
   def new_host_en
     host = Host.last
-    HostMailer.new_host(host.user.id, 'en')
+    HostMailer.new_host(host.user.id, :en)
   end
 
   def new_host_he
     host = Host.last
-    HostMailer.new_host(host.user.id, 'he')
+    HostMailer.new_host(host.user.id, :he)
   end
 
   def guest_cancelled_request_en
     invite_id = Invite.first.idrequest_rejectedrequest_rejected
-    RequestMailer.guest_cancelled_request(invite_id, 'en')
+    RequestMailer.guest_cancelled_request(invite_id, :en)
   end
 
   def guest_cancelled_request_he
     invite_id = Invite.first.id
-    RequestMailer.guest_cancelled_request(invite_id, 'he')
+    RequestMailer.guest_cancelled_request(invite_id, :he)
   end
 
   def host_rejected_request_en
     invite_id = Invite.first.id
-    RequestMailer.request_rejected(invite_id, 'en')
+    RequestMailer.request_rejected(invite_id, :en)
   end
 
   def host_rejected_request_he
     invite_id = Invite.first.id
-    RequestMailer.request_rejected(invite_id, 'he')
+    RequestMailer.request_rejected(invite_id, :he)
   end
 
   def guest_send_request_en
     invite_id = Invite.first.id
-    RequestMailer.new_guest(invite_id, 'en')
+    RequestMailer.new_guest(invite_id, :en)
   end
 
   def guest_send_request_he
     invite_id = Invite.first.id
-    RequestMailer.new_guest(invite_id, 'he')
+    RequestMailer.new_guest(invite_id, :he)
   end
 
   def pending_invite_received_en
     invite_id = Invite.first.id
-    RequestMailer.pending_invite_received(invite_id, 'en')
+    RequestMailer.pending_invite_received(invite_id, :en)
   end
 
   def pending_invite_received_he
     invite_id = Invite.first.id
-    RequestMailer.pending_invite_received(invite_id, 'he')
+    RequestMailer.pending_invite_received(invite_id, :he)
   end
 
 
@@ -66,25 +66,25 @@ class MailPreview < MailView
   def witness_assigned_survivor_he
     host_id = Host.last.id
     witness_id = Witness.where(:witness_type => :survivor).last
-    HostMailer.witness_assigned(host_id, witness_id, 'he')
+    HostMailer.witness_assigned(host_id, witness_id, :he)
   end
 
   def witness_assigned_academia_he
     host_id = Host.last.id
     witness_id = Witness.where(:witness_type => :academia).last
-    HostMailer.witness_assigned(host_id, witness_id, 'he')
+    HostMailer.witness_assigned(host_id, witness_id, :he)
   end
 
   def witness_assigned_second_generation_he
     host_id = Host.last.id
     witness_id = Witness.where(:witness_type => :second_generation).last
-    HostMailer.witness_assigned(host_id, witness_id, 'he')
+    HostMailer.witness_assigned(host_id, witness_id, :he)
   end
 
   def witness_assigned_therapist_he
     host_id = Host.last.id
     witness_id = Witness.where(:witness_type => :therapist).last
-    HostMailer.witness_assigned(host_id, witness_id, 'he')
+    HostMailer.witness_assigned(host_id, witness_id, :he)
   end
 
 end
