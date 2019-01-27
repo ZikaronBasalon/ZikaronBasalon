3d496fc1fe56f262f4243e100e6c983e115e86c3
Aryeh Beitz <aryehbeitz@gmail.com>
2019-01-27 23:01:06 +0200

    fix witness save

--- app/controllers/witnesses_controller.rb
+++ app/controllers/witnesses_controller.rb
@@ -59,20 +59,24 @@ class WitnessesController < ApplicationController
     end
   end
 
+  def safe_params
+    params.require(:witness).permit(:full_name, :address, :city_id, :witness_type, :language, :email, :phone, :stairs, :special_needs, :seminar_required, :free_text, :special_population, :contacted, :contacted_by_host, :available_for_teaming, :host_id, :concept, :can_morning, :can_afternoon, :can_evening, :free_on_day, :contact_name, :contact_phone, :external_assignment, :additional_phone, :available_day1, :available_day2, :available_day3, :available_day4, :available_day5, :available_day6, :archived, :need_to_followup, :active_last_year, :available_day7, :available_day8, :available_day9)
+  end
+
   # PUT /witnesses/1
   # PUT /witnesses/1.json
   def update
     @witness = Witness.find(params[:id])
 
     respond_to do |format|
-      if @witness.update_attributes(params[:witness])
-         if(params[:witness][:host_id].present?)
+      if @witness.update_columns(safe_params)
+         if(safe_params[:host_id].present?)
           HostMailer.witness_assigned(
-            params[:witness][:host_id],
+            safe_params[:host_id],
             @witness.id,
             I18n.locale
           ).deliver
-          @host = Host.find(params[:witness][:host_id])
+          @host = Host.find(safe_params[:host_id])
           @host.update_attributes(assignment_time: Time.now.utc.localtime, witness_id: params[:id])
         end
 
