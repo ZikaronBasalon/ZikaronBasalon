10ebeb31996911fa9cb519dd6e6cd057e9175f7e
Aryeh Beitz <aryehbeitz@gmail.com>
2019-03-08 00:19:22 +0200

    add object changes to versions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306222341) do
+ActiveRecord::Schema.define(version: 20190307221712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -216,6 +216,7 @@ ActiveRecord::Schema.define(version: 20190306222341) do
     t.text     "object"
     t.datetime "created_at"
     t.integer  "transaction_id"
+    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
